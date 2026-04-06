f159af4b81a8d840658f84801e49f5089976edef
R0F7 <wwwrafikhan075@gmail.com>
2026-04-06 20:11:23 +0600

    add front end deploy Url and vercel.json

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://buddy-script-client-seven.vercel.app",
+  ],
   credentials: true,
   optionSuccessStatus: 200,
 };
